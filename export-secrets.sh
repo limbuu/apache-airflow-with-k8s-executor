0336d31c7010a224e0799f4a0395b25453d889b6
Manshi Limbu <merosansar116@gmail.com>
2021-04-07 00:33:10 +0545

    Update export-secrets.sh

--- export-secrets.sh
+++ export-secrets.sh
@@ -1,5 +1,4 @@
 #!/bin/bash
-echo "hello world"
 export AIRFLOW_PASSWORD=$(kubectl get secret --namespace default airflow -o jsonpath="{.data.airflow-password}" | base64 --decode)
 export AIRFLOW_FERNETKEY=$(kubectl get secret --namespace default airflow -o jsonpath="{.data.airflow-fernetKey}" | base64 --decode)
 export POSTGRESQL_PASSWORD=$(kubectl get secret --namespace default airflow-postgresql -o jsonpath="{.data.postgresql-password}" | base64 --decode)
